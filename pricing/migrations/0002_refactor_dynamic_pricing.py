afae175629c01714b8e9faac0b0380341bff8ca6
ibrodevs <ibroabdraimov20@gmail.com>
2026-06-13 21:08:01 +0600

    sdf

--- pricing/migrations/0002_refactor_dynamic_pricing.py
+++ pricing/migrations/0002_refactor_dynamic_pricing.py
@@ -89,7 +89,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='season',
             name='code',
-            field=models.SlugField(blank=True, max_length=50, null=True),
+            field=models.SlugField(blank=True, db_index=False, max_length=50, null=True),
         ),
         migrations.AddField(
             model_name='season',
